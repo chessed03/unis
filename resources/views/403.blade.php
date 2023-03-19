f1255e77ec685472520f798b78c00eb35fe4fd86
chessed03 <chessed03@gmail.com>
2023-03-19 11:23:18 -0600

    test

--- resources/views/403.blade.php
+++ resources/views/403.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8 mt-5">
                 <div class="card">
-                    <div class="card-header">Área no autorizada</div>
+                    <div class="card-header">Área no autorizada.</div>
 
                     <div class="card-body">
 
